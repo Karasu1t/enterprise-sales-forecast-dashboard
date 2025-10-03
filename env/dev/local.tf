994ddf938b0f23de6a170a367d4be0718ae06686
Masaru Ogasawara <massaru44@gmail.com>
2025-10-03 16:34:13 +0900

    Delete env/dev/local.tf

--- env/dev/local.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  id              = "karasuit"
-  project         = "buoyant-sunbeam-461215-p9"
-  environment     = "dev"
-  gcp_region      = "asia-northeast1"
-  gcp_zone        = "asia-northeast1-a"
-  display_name    = "sales_data_repo"
-  repository_name = "sales_data_repo"
-}
