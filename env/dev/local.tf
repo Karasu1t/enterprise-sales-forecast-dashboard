ef73e75e42599669c43741b089107138cae896ed
Masaru Ogasawara <massaru44@gmail.com>
2025-12-06 01:28:30 +0900

    add_CICD

---
+++ env/dev/local.tf
@@ -0,0 +1,7 @@
+variable "id" {}
+variable "project" {}
+variable "environment" {}
+variable "gcp_region" {}
+variable "gcp_zone" {}
+variable "email" {}
+variable "email_mas" {}
--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,29 +1,29 @@
 # Storage
 module "storage" {
   source      = "../../modules/storage"
-  id          = local.id
-  project     = local.project
-  environment = local.environment
+  id          = var.id
+  project     = var.project
+  environment = var.environment
 }
 
 # bq
 module "bq" {
   source      = "../../modules/bq"
-  id          = local.id
-  environment = local.environment
-  project     = local.project
-  email       = local.email
-  email_mas   = local.email_mas
+  id          = var.id
+  environment = var.environment
+  project     = var.project
+  email       = var.email
+  email_mas   = var.email_mas
 }
 
 # Function
 module "function" {
   source         = "../../modules/functions"
-  id             = local.id
-  project        = local.project
-  environment    = local.environment
-  gcp_region     = local.gcp_region
-  email          = local.email
+  id             = var.id
+  project        = var.project
+  environment    = var.environment
+  gcp_region     = var.gcp_region
+  email          = var.email
   dataset        = module.bq.dataset
   source_bucket  = module.storage.import_script
   trigger_bucket = module.storage.salesdata
@@ -32,8 +32,8 @@ module "function" {
 # Artifact Registry
 module "artifactregistry" {
   source        = "../../modules/artifact_registry"
-  project       = local.project
-  location      = local.gcp_region
+  project       = var.project
+  location      = var.gcp_region
   repository_id = "sales-data-prediction-model-repo"
 }
 
