2ba839fdb6ed27b30868b94433e32f01cb078aad
Masaru Ogasawara <massaru44@gmail.com>
2025-12-06 20:56:37 +0900

    learning_vertexAI

--- modules/artifact_registry/artifact_registry.tf
+++ modules/artifact_registry/artifact_registry.tf
@@ -4,3 +4,10 @@ resource "google_artifact_registry_repository" "sales_data_prediction_model_repo
   repository_id = var.repository_id
   format        = "DOCKER"
 }
+
+resource "google_artifact_registry_repository" "sales_data_learning_model_repo" {
+  project       = var.project
+  location      = var.location
+  repository_id = var.repository_id
+  format        = "DOCKER"
+}
