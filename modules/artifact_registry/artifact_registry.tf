98b2ec8b91ef6f4d0cfa7fa2d04652523860773e
Masaru Ogasawara <massaru44@gmail.com>
2025-12-06 21:00:43 +0900

    learning_vertexAI

--- modules/artifact_registry/artifact_registry.tf
+++ modules/artifact_registry/artifact_registry.tf
@@ -1,10 +1,3 @@
-resource "google_artifact_registry_repository" "sales_data_prediction_model_repo" {
-  project       = var.project
-  location      = var.location
-  repository_id = var.repository_id
-  format        = "DOCKER"
-}
-
 resource "google_artifact_registry_repository" "sales_data_learning_model_repo" {
   project       = var.project
   location      = var.location
--- modules/artifact_registry/outputs.tf
+++ modules/artifact_registry/outputs.tf
@@ -1,7 +1,7 @@
 output "repository_id" {
-  value = google_artifact_registry_repository.sales_data_prediction_model_repo.repository_id
+  value = google_artifact_registry_repository.sales_data_learning_model_repo.repository_id
 }
 
 output "repository_url" {
-  value = google_artifact_registry_repository.sales_data_prediction_model_repo.id
+  value = google_artifact_registry_repository.sales_data_learning_model_repo.id
 }
