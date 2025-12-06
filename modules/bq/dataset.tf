68386b61078bbc8bb58487922650138fbcda8125
Masaru Ogasawara <massaru44@gmail.com>
2025-12-06 21:38:26 +0900

    learning_vertexAI

--- modules/bq/dataset.tf
+++ modules/bq/dataset.tf
@@ -14,3 +14,22 @@ resource "google_bigquery_dataset" "dataset" {
     user_by_email = var.email_mas
   }
 }
+
+resource "google_bigquery_table" "sales_raw" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = "sales_raw"
+  schema = jsonencode([
+    { name = "date", type = "DATE", mode = "REQUIRED" },
+    { name = "product_name", type = "STRING", mode = "REQUIRED" },
+    { name = "quantity", type = "INT64", mode = "NULLABLE" },
+    { name = "price", type = "INT64", mode = "NULLABLE" },
+    { name = "sales", type = "INT64", mode = "NULLABLE" },
+    { name = "weather", type = "STRING", mode = "NULLABLE" },
+    { name = "holiday_flag", type = "INT64", mode = "NULLABLE" }
+  ])
+  time_partitioning {
+    type  = "DAY"
+    field = "date"
+  }
+  deletion_protection = false
+}
