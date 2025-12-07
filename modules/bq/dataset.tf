ef7c051acebdd8189411ee9a29bbc1f6f381ec80
Masaru Ogasawara <massaru44@gmail.com>
2025-12-07 16:45:07 +0900

    modeling_vertexAI

--- modules/bq/dataset.tf
+++ modules/bq/dataset.tf
@@ -2,7 +2,6 @@ resource "google_bigquery_dataset" "dataset" {
   dataset_id                  = "${var.environment}_${var.id}_sales"
   friendly_name               = "test"
   location                    = "ASIA-NORTHEAST1"
-  default_table_expiration_ms = 3600000
 
   access {
     role          = "OWNER"
